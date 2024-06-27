733b29bc7a23dd0d4fcf869f7eee2a70a86fe6e6
dreamqip <v.chuchupak@gmail.com>
2024-06-28 00:20:33 +0200

    feat: rebranding

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import netlify from "@astrojs/netlify";
 import react from "@astrojs/react";
-import { sanityIntegration } from "@sanity/astro";
+import sanityIntegration from "@sanity/astro";
 
 import { loadEnv } from "vite";
 
@@ -16,15 +16,15 @@ export const { PUBLIC_SANITY_PROJECT_ID, PUBLIC_SANITY_DATASET } = loadEnv(
 export default defineConfig({
   integrations: [
     tailwind(),
-    sanityIntegration({
-      projectId: PUBLIC_SANITY_PROJECT_ID,
-      dataset: PUBLIC_SANITY_DATASET,
-      // Set useCdn to false if you're building statically.
-      useCdn: false,
-      studioBasePath: "/admin",
-      apiVersion: "2023-03-20",
-    }),
-    react(),
+    // sanityIntegration({
+    //   projectId: PUBLIC_SANITY_PROJECT_ID,
+    //   dataset: PUBLIC_SANITY_DATASET,
+    //   // Set useCdn to false if you're building statically.
+    //   useCdn: false,
+    //   studioBasePath: "/admin",
+    //   apiVersion: "2023-03-20",
+    // }),
+    // react(),
   ],
   output: "hybrid",
   adapter: netlify(),
